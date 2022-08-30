b3220e16664fc9975f9dd21cb8f76d55cc511fbd
Joris Borgdorff <joris@thehyve.nl>
2022-08-30 17:15:03 +0200

    Configure ksqlDB queries and jdbc connectors

---
+++ etc/outcomes-ksql-transform/queries.sql
@@ -0,0 +1,72 @@
+-- Define stream from questionnaire response topic
+-- This way it can be read by ksqlDB
+CREATE STREAM questionnaire_response (
+    projectId VARCHAR KEY,
+    userId VARCHAR KEY,
+    sourceId VARCHAR KEY,
+    time DOUBLE,
+    timeCompleted DOUBLE,
+    timeNotification DOUBLE,
+		name VARCHAR,
+		version VARCHAR,
+    answers ARRAY<STRUCT<questionId VARCHAR, value STRUCT<int INT, string VARCHAR, double DOUBLE>, startTime DOUBLE, endTime DOUBLE>>
+) WITH (
+    kafka_topic = 'questionnaire_response',
+    partitions = 3,
+    format = 'avro'
+);
+
+-- Define table loaded from the outcomes_dashboard_variables table.
+-- This way it can be read by ksqlDB
+CREATE TABLE outcomes_dashboard_variables (
+    id BIGINT,
+    name VARCHAR PRIMARY KEY,
+    type VARCHAR,
+    category VARCHAR,
+    date_type VARCHAR
+) WITH (
+    kafka_topic = 'outcomes_dashboard_variables',
+    partitions = 3,
+    format = 'avro'
+);
+
+-- Convert all questionnaires answers to single observation records
+CREATE STREAM outcomes_observations_unindexed
+WITH (
+    kafka_topic = 'ksql_outcomes_observations_unindexed',
+    partitions = 3,
+    format = 'avro'
+)
+AS SELECT
+    EXPLODE(TRANSFORM(q.answers, a => a->questionId)) as variable,
+    FROM_UNIXTIME(CAST(q.time * 1000 AS BIGINT)) as date,
+    q.userId as subject_id,
+    CAST(NULL as TIMESTAMP) as end_date,
+    -- if present, take the int response, otherwise try to convert if the string response to a numeric value.
+    EXPLODE(TRANSFORM(q.answers, a => COALESCE(a->value->int, CAST(a->value->string as INT)))) as value_numeric,
+    EXPLODE(TRANSFORM(q.answers, a => a->value->string)) as value_textual
+FROM questionnaire_response q
+PARTITION BY q.userId
+EMIT CHANGES;
+
+-- Read the correct variable ID from the database and join it with the question ID.
+-- Non-existing variables will not be joined so questionIds that are not used in H2O
+-- will not be converted.
+CREATE STREAM outcomes_observations
+WITH (
+    kafka_topic = 'ksql_outcomes_observations',
+    partitions = 3,
+    format = 'avro'
+)
+AS SELECT
+    o.variable,
+    v.id as variable_id,
+    o.subject_id,
+    o.date,
+    o.end_date,
+    o.value_numeric,
+    o.value_textual
+FROM outcomes_observations_unindexed o
+-- this will force o.variable to be the record key
+JOIN outcomes_dashboard_variables v ON o.variable = v.name
+EMIT CHANGES;
